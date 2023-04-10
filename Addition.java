f841a607f7c6b5af4e0c9f7b5ebe2ff574a5ef42
Dev23199 <115926085+Dev23199@users.noreply.github.com>
2023-04-10 20:39:25 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Addition{
+    public static void main(String args[]){
+
+        int i=10;
+        int j=20;
+        int sum=i+j;
+
+        System.out.println(sum);
+    }
+}
