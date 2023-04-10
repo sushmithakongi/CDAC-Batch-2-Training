922a019de5c2897d4674568c5bc79706e77cbfe1
shruti <shrutidhore18@gmail.com>
2023-04-10 16:47:34 +0530

    additon of two number

---
+++ addition.java
@@ -0,0 +1,9 @@
+public class addition{
+    public static void main(String args[])
+    {
+        int i=20;
+        int j=30;
+        int sum =i+j;
+        System.out.println("Sum is" +sum);
+    }
+}
