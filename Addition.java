54c8e0d4728912e5249ca6c5f9a0f7088a147ffa
nikhilzambare24 <nikhilzambare24@gmail.com>
2023-04-10 12:39:29 +0530

    Java file created for addition

---
+++ Addition.java
@@ -0,0 +1,11 @@
+public class Addition {
+    public static void main(String[] args) {
+
+        int i = 5;
+        int j = 10;
+
+        int sum = i + j;
+
+        System.out.println(sum);
+    }
+}
