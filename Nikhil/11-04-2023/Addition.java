fa870917a738d5b8d31a4097be5b6967b058c2a2
nikhilzambare24 <nikhilzambare24@gmail.com>
2023-04-10 18:28:28 +0530

    Added in Nikhil folder new

---
+++ Nikhil/11-04-2023/Addition.java
@@ -0,0 +1,11 @@
+public class Addition {
+    public static void main(String[] args) {
+
+        int i = 5;
+        int j = 20;
+
+        int sum = i + j;
+
+        System.out.println(sum);
+    }
+}
