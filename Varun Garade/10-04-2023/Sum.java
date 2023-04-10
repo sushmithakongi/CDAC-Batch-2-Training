1b083a40c56a1f388e497fd229c0f213072c1cb7
Varun <varungarade1151@gmail.com>
2023-04-10 22:37:49 +0530

    sum

---
+++ Varun Garade/10-04-2023/Sum.java
@@ -0,0 +1,10 @@
+import javax.swing.plaf.synth.SynthStyleFactory;
+
+public class Sum {
+
+    public static void main(String[] args) {
+        int a= 10;
+        int b = 15;
+        System.out.println("Sum is : "+a+b);
+    }
+}
