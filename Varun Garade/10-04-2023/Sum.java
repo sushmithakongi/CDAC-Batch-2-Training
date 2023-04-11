e75a3c0777cf7eea74599bf54892df4d7fd7edcd
Yashwant <yashrathore5598@gmail.com>
2023-04-11 15:51:57 +0530

    till 11 April

--- Varun Garade/10-04-2023/Sum.java
+++
@@ -1,9 +0,0 @@
-public class Sum {
-
-    public static void main(String[] args) {
-        int a= 10;
-        int b = 15;
-        System.out.println("Sum is : "+a+b);
-        System.out.println("Second");
-    }
-}   
