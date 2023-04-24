797989d330c3acdaba071d498d0983d44bc9dc31
shubham <sb11061997@gmail.com>
2023-04-24 16:27:52 +0530

    24

--- Varun Garade/20-04-2023/17_print_lines.sh
+++ Varun Garade/20-04-2023/17_print_lines.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# check if the file name is provided as an argument
+
 if [ $# -lt 3 ]
   then
     echo "Error: missing arguments"
@@ -8,11 +8,11 @@ if [ $# -lt 3 ]
     exit 1
 fi
 
-# get the arguments
+
 file="$1"
 start="$2"
 num="$3"
 
-# use sed to print the desired lines
+
 tail -n +$start "$file" | head -n $num
 
